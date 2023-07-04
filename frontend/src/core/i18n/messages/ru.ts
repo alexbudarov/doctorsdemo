37ca91b15f39880969e01dcb4058ce490664001c
budarov <budarov@haulmont.com>
2023-07-04 19:00:38 +0400

    add russian captions (fix)

--- frontend/src/core/i18n/messages/ru.ts
+++ frontend/src/core/i18n/messages/ru.ts
@@ -22,17 +22,16 @@ export const ru: TranslationMessages = {
         lastName: "Фамилия"
       }
     },
-
-    enums: {
-      Specialty: {
-        ALLERGY_AND_IMMUNOLOGY: "Аллергология и иммунология",
-        DERMATOLOGY: "Дерматология",
-        GP: "Общая практика",
-        NEUROLOGY: "Неврология",
-        OPHTHALMOLOGY: "Офтальмология",
-        PSYCHIATRY: "Психиатрия"
-      }
-    }
   },
-  // place for your messages
+
+  enums: {
+    Specialty: {
+      ALLERGY_AND_IMMUNOLOGY: "Аллергология и иммунология",
+      DERMATOLOGY: "Дерматология",
+      GP: "Общая практика",
+      NEUROLOGY: "Неврология",
+      OPHTHALMOLOGY: "Офтальмология",
+      PSYCHIATRY: "Психиатрия"
+    }
+  }  // place for your messages
 };
